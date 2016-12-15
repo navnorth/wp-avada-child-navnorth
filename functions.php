664560d70a1cbe3c00b3aefc242baa2aa9d17d60
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2016-12-15 22:44:57 +0800

    fix pagination issue on "News" slug

--- functions.php
+++ functions.php
@@ -30,8 +30,27 @@ function my_NEWS_default_post_type() {
         'capability_type' => 'post',
         'map_meta_cap' => true,
         'hierarchical' => false,
-        'rewrite' => array( 'slug' => 'news', 'with_front' => false ),
+        'rewrite' => array( 'slug' => 'news', 'with_front' => false, 'pagination' => true ),
         'query_var' => false,
         'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'post-formats' ),
     ) );
+}
+
+//Add rewrite tag use for news permalinks
+add_action('init', 'my_NEWS_rewrite_tag', 10, 0);
+function my_NEWS_rewrite_tag() {
+    add_rewrite_tag( '%news%', '([^&]+)' );
+}
+
+//Add rewrite rule for News
+add_action( 'init', 'my_NEWS_rewrite_rules', 10, 0 );
+function my_NEWS_rewrite_rules(){
+    add_rewrite_rule( '^news/page/([0-9]{1,})/?', 'index.php?post_type=post&paged=$matches[1]', 'top' );
+}
+
+//Flush rewrite rules after activating child theme
+add_action( 'after_switch_theme', 'my_NEWS_rewrite_flush' );
+function my_NEWS_rewrite_flush() {
+    my_NEWS_default_post_type();
+    flush_rewrite_rules();
 }
